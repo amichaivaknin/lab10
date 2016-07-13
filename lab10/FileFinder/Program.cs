f87a122e5eecfd212b31750167755afad1dd2697
AmichaiVak <amiko565@gmail.com>
2016-07-13 11:32:33 +0300

    Add files via upload

---
+++ lab10/FileFinder/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFinder
+{
+    class Program
+    {
+        private static string _search;
+        private static List<string> _files=new List<string>();
+
+        private static void CheckFile(string fileName)
+        {
+            try
+            {
+                var reader = new StreamReader(fileName);
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        return;
+                    }
+                    if (!line.Contains(_search)) continue;
+                    FileInfo fileInfo = new FileInfo(fileName);
+                    _files.Add("Name: "+fileInfo.Name+" Length: "+ fileInfo.Length);
+                    return;
+                }
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+////////////////////////////////////////////////////////////////////////////////
+#if DEBUG
+            args = new[] {$@"C:\Users\Ami\Documents\Visual Studio 2015\Projects\codeValue\lab10\Personnel\bin\Debug", "Simpson" };
+#endif
+////////////////////////////////////////////////////////////////////////////////
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Parameter missing");
+                return;
+            }
+
+            string directory = args[0];
+            _search = args[1];
+            if (Directory.Exists(directory))
+            {
+                string[] fileEntries = Directory.GetFiles(directory, "*.txt");
+                foreach (string fileName in fileEntries)
+                {
+                    CheckFile(fileName);
+                }
+
+                foreach (var file in _files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong path or directory not found");
+            }
+        }
+    }
+}
---
+++ lab10/Personnel/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class Program
+    {
+        public static void ReadFile(List<string> personnels)
+        {
+            try
+            {
+                var reader = new StreamReader("PersonnelFile1.txt");
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        return;
+                    }
+                    personnels.Add(line);
+                }
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            var personnels =  new List<string>();
+            ReadFile(personnels);
+            foreach (var personnel in personnels)
+            {
+                Console.WriteLine(personnel);
+            }
+        }
+    }
+}
